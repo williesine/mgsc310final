488595a1612c90028d9505a12b901bb9dd44608d
CamEmamian <102640932+CamEmamian@users.noreply.github.com>
2022-11-29 13:35:22 -0800

    Update 310_Final_Project.R

--- 310_Final_Project.R
+++ 310_Final_Project.R
@@ -6,5 +6,293 @@ library('devtools')
 install_github("BillPetti/baseballr")
 
 date1 = baseballr::scrape_statcast_savant(start_date = '2022-04-07',
-                                          end_date = '2022-04-17', 
+                                          end_date = '2022-04-10', 
                                           player_type = 'pitcher')
+
+date2 = baseballr::scrape_statcast_savant(start_date = '2022-04-11',
+                                          end_date = '2022-04-14', 
+                                          player_type = 'pitcher')
+
+date3 = baseballr::scrape_statcast_savant(start_date = '2022-04-15',
+                                          end_date = '2022-04-18', 
+                                          player_type = 'pitcher')
+
+date4 = baseballr::scrape_statcast_savant(start_date = '2022-04-19',
+                                          end_date = '2022-04-21', 
+                                          player_type = 'pitcher')
+
+date5 = baseballr::scrape_statcast_savant(start_date = '2022-04-22',
+                                          end_date = '2022-04-25', 
+                                          player_type = 'pitcher')
+
+date6 = baseballr::scrape_statcast_savant(start_date = '2022-04-26',
+                                           end_date = '2022-04-28', 
+                                           player_type = 'pitcher')
+
+date7 = baseballr::scrape_statcast_savant(start_date = '2022-04-29',
+                                           end_date = '2022-05-01', 
+                                           player_type = 'pitcher')
+
+date8 = baseballr::scrape_statcast_savant(start_date = '2022-05-02',
+                                          end_date = '2022-05-05', 
+                                          player_type = 'pitcher')
+
+date9 = baseballr::scrape_statcast_savant(start_date = '2022-05-06',
+                                           end_date = '2022-05-08', 
+                                           player_type = 'pitcher')
+
+date10 = baseballr::scrape_statcast_savant(start_date = '2022-05-09',
+                                          end_date = '2022-05-11', 
+                                          player_type = 'pitcher')
+
+date11 = baseballr::scrape_statcast_savant(start_date = '2022-05-12',
+                                          end_date = '2022-05-14', 
+                                          player_type = 'pitcher')
+
+date12 = baseballr::scrape_statcast_savant(start_date = '2022-05-15',
+                                          end_date = '2022-05-17', 
+                                          player_type = 'pitcher')
+
+date13 = baseballr::scrape_statcast_savant(start_date = '2022-05-18',
+                                          end_date = '2022-05-20', 
+                                          player_type = 'pitcher')
+
+date14 = baseballr::scrape_statcast_savant(start_date = '2022-05-21',
+                                         end_date = '2022-05-23', 
+                                         player_type = 'pitcher')
+
+date15 = baseballr::scrape_statcast_savant(start_date = '2022-05-24',
+                                          end_date = '2022-05-26', 
+                                          player_type = 'pitcher')
+
+date16 = baseballr::scrape_statcast_savant(start_date = '2022-05-27',
+                                          end_date = '2022-05-29', 
+                                          player_type = 'pitcher')
+
+date17 = baseballr::scrape_statcast_savant(start_date = '2022-05-30',
+                                         end_date = '2022-06-01', 
+                                         player_type = 'pitcher')
+# -----------------------------------------------------------------------
+#begin with date 33 entering june
+date18 = baseballr::scrape_statcast_savant(start_date = '2022-06-01',
+                                           end_date = '2022-06-03', 
+                                           player_type = 'pitcher')
+
+date19 = baseballr::scrape_statcast_savant(start_date = '2022-06-04',
+                                          end_date = '2022-06-06', 
+                                          player_type = 'pitcher')
+
+date20 = baseballr::scrape_statcast_savant(start_date = '2022-06-07',
+                                          end_date = '2022-06-09', 
+                                          player_type = 'pitcher')
+
+date21 = baseballr::scrape_statcast_savant(start_date = '2022-06-10',
+                                          end_date = '2022-06-12', 
+                                          player_type = 'pitcher')
+
+date22 = baseballr::scrape_statcast_savant(start_date = '2022-06-13',
+                                           end_date = '2022-06-15', 
+                                           player_type = 'pitcher')
+
+date23 = baseballr::scrape_statcast_savant(start_date = '2022-06-16',
+                                           end_date = '2022-06-18', 
+                                           player_type = 'pitcher')
+
+date24 = baseballr::scrape_statcast_savant(start_date = '2022-06-19',
+                                           end_date = '2022-06-21', 
+                                           player_type = 'pitcher')
+
+date25 = baseballr::scrape_statcast_savant(start_date = '2022-06-22',
+                                           end_date = '2022-06-24', 
+                                           player_type = 'pitcher')
+
+date26 = baseballr::scrape_statcast_savant(start_date = '2022-06-25',
+                                           end_date = '2022-06-27', 
+                                           player_type = 'pitcher')
+
+date27 = baseballr::scrape_statcast_savant(start_date = '2022-06-28',
+                                           end_date = '2022-06-30', 
+                                           player_type = 'pitcher')
+#----------------------------------------------------------------------
+#July
+
+date28 = baseballr::scrape_statcast_savant(start_date = '2022-07-01',
+                                          end_date = '2022-07-03', 
+                                          player_type = 'pitcher')
+
+date29 = baseballr::scrape_statcast_savant(start_date = '2022-07-04',
+                                           end_date = '2022-07-06', 
+                                           player_type = 'pitcher')
+
+date30 = baseballr::scrape_statcast_savant(start_date = '2022-07-07',
+                                           end_date = '2022-07-09', 
+                                           player_type = 'pitcher')
+
+date31 = baseballr::scrape_statcast_savant(start_date = '2022-07-10',
+                                           end_date = '2022-07-12', 
+                                           player_type = 'pitcher')
+
+date32 = baseballr::scrape_statcast_savant(start_date = '2022-07-13',
+                                           end_date = '2022-07-15', 
+                                           player_type = 'pitcher')
+
+date33 = baseballr::scrape_statcast_savant(start_date = '2022-07-16',
+                                           end_date = '2022-07-18', 
+                                           player_type = 'pitcher')
+
+date34 = baseballr::scrape_statcast_savant(start_date = '2022-07-19',
+                                           end_date = '2022-07-21', 
+                                           player_type = 'pitcher')
+
+date35 = baseballr::scrape_statcast_savant(start_date = '2022-07-22',
+                                           end_date = '2022-07-24', 
+                                           player_type = 'pitcher')
+
+date36 = baseballr::scrape_statcast_savant(start_date = '2022-07-25',
+                                           end_date = '2022-07-27', 
+                                           player_type = 'pitcher')
+
+date37 = baseballr::scrape_statcast_savant(start_date = '2022-07-28',
+                                           end_date = '2022-07-30', 
+                                           player_type = 'pitcher')
+#---------------------------------------------------------------------
+#August
+
+date38 = baseballr::scrape_statcast_savant(start_date = '2022-07-31',
+                                           end_date = '2022-08-02', 
+                                           player_type = 'pitcher')
+
+date39 = baseballr::scrape_statcast_savant(start_date = '2022-08-03',
+                                         end_date = '2022-08-05', 
+                                         player_type = 'pitcher')
+
+date40 = baseballr::scrape_statcast_savant(start_date = '2022-08-06',
+                                           end_date = '2022-08-08', 
+                                           player_type = 'pitcher')
+
+date41 = baseballr::scrape_statcast_savant(start_date = '2022-08-09',
+                                           end_date = '2022-08-11', 
+                                           player_type = 'pitcher')
+
+date42 = baseballr::scrape_statcast_savant(start_date = '2022-08-12',
+                                           end_date = '2022-08-14', 
+                                           player_type = 'pitcher')
+
+date43 = baseballr::scrape_statcast_savant(start_date = '2022-08-15',
+                                           end_date = '2022-08-17', 
+                                           player_type = 'pitcher')
+
+date44 = baseballr::scrape_statcast_savant(start_date = '2022-08-18',
+                                           end_date = '2022-08-20', 
+                                           player_type = 'pitcher')
+
+date45 = baseballr::scrape_statcast_savant(start_date = '2022-08-21',
+                                           end_date = '2022-08-23', 
+                                           player_type = 'pitcher')
+
+date46 = baseballr::scrape_statcast_savant(start_date = '2022-08-24',
+                                           end_date = '2022-08-26', 
+                                           player_type = 'pitcher')
+
+date47 = baseballr::scrape_statcast_savant(start_date = '2022-08-27',
+                                           end_date = '2022-08-29', 
+                                           player_type = 'pitcher')
+
+date47 = baseballr::scrape_statcast_savant(start_date = '2022-08-30',
+                                           end_date = '2022-09-01', 
+                                           player_type = 'pitcher')
+
+#-----------------------------------------------------------------------
+#September
+
+date48 = baseballr::scrape_statcast_savant(start_date = '2022-09-02',
+                                           end_date = '2022-09-04', 
+                                           player_type = 'pitcher')
+
+date49 = baseballr::scrape_statcast_savant(start_date = '2022-09-05',
+                                           end_date = '2022-09-07', 
+                                           player_type = 'pitcher')
+
+date50 = baseballr::scrape_statcast_savant(start_date = '2022-09-08',
+                                           end_date = '2022-09-10', 
+                                           player_type = 'pitcher')
+
+date51 = baseballr::scrape_statcast_savant(start_date = '2022-09-11',
+                                           end_date = '2022-09-13', 
+                                           player_type = 'pitcher')
+
+date52 = baseballr::scrape_statcast_savant(start_date = '2022-09-14',
+                                           end_date = '2022-09-16', 
+                                           player_type = 'pitcher')
+
+date53 = baseballr::scrape_statcast_savant(start_date = '2022-09-17',
+                                           end_date = '2022-09-19', 
+                                           player_type = 'pitcher')
+
+date54 = baseballr::scrape_statcast_savant(start_date = '2022-09-20',
+                                           end_date = '2022-09-22', 
+                                           player_type = 'pitcher')
+
+date55 = baseballr::scrape_statcast_savant(start_date = '2022-09-23',
+                                           end_date = '2022-09-25', 
+                                           player_type = 'pitcher')
+
+date56 = baseballr::scrape_statcast_savant(start_date = '2022-09-26',
+                                           end_date = '2022-09-27', 
+                                           player_type = 'pitcher')
+
+date57 = baseballr::scrape_statcast_savant(start_date = '2022-09-28',
+                                           end_date = '2022-09-30', 
+                                           player_type = 'pitcher')
+
+date58 = baseballr::scrape_statcast_savant(start_date = '2022-10-01',
+                                         end_date = '2022-10-03', 
+                                         player_type = 'pitcher')
+SavantData22 = rbind(date1, date10, date11, date12, date13, date14, date15, 
+                     date16, date17, date18, date19, date2, date20, date21, 
+                     date22,date23, date24, date25, date26, date27, date28, 
+                     date29, date3, date30, date31, date32, date33, date34, 
+                     date35, date36, date37, date38, date39, date4, date40,
+                     date41, date42, date43, date44, date45, date46, date47,
+                     date48, date49, date5, date50, date51, date52, date53,
+                     date54, date55, date56, date57, date58, date6, date7, 
+                     date8, date9)
+
+SavantOBS = SavantData22[sample(nrow(SavantData22), 10),]
+
+write.csv(SavantData22, "SavantPitchingData2022.csv", row.names = F)
+
+SavantData22 = read.csv("SavantPitchingData2022.csv", stringsAsFactors = F)
+
+variables_df <- SavantData22 %>% select(player_name, pitch_name, release_speed, 
+                                        release_pos_x, release_pos_z, description,
+                                        zone, stand, p_throws, type, pfx_x, pfx_z,
+                                        plate_x, plate_z, effective_speed, 
+                                        release_spin_rate, release_extension,
+                                        pitcher, release_pos_y, 
+                                        spin_axis)
+
+names_change_df <- rename(variables_df, horiz_release = release_pos_x , 
+                                           vert_release = release_pos_z , 
+                                           dist_from_home = release_pos_y , 
+                          batter_side = stand , pitcher_throws = p_throws , 
+                          ball_strike_in_play = type , horiz_break = pfx_x ,
+                          vert_break = pfx_z , horiz_pos = plate_x , 
+                          vert_pos = plate_z)
+
+statcast_clean <- names_change_df %>% na.omit()
+
+
+summary(statcast_clean)
+
+checkUnique <- unique(statcast_clean$description)
+checkUnique
+
+#Calculate Whiff
+whiff <- ifelse(statcast_clean$description == "swinging_strike" | 
+                  statcast_clean$description == "foul_tip" |
+                  statcast_clean$description == "bunt_foul_tip" | 
+                  statcast_clean$description == "swinging_strike_blocked" |
+                  statcast_clean$description == "missed_bunt" , 1, 0)
+
+statcast_clean$whiff <- whiff
